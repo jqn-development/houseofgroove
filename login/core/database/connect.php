912a26a430d4324fff0d5d956023687cf9604f55
jQN <owner@Owners-MacBook-Pro.local>
2015-04-21 01:24:13 -0600

    Updated services icon for tablet

--- login/core/database/connect.php
+++ login/core/database/connect.php
@@ -1,11 +1,11 @@
 <?php
   //Local
-  // $connect_error = 'Sorry, we\'re experiencing connection issues.';
-  // mysql_connect('localhost', 'root', 'root') or die($connect_error);
-  // mysql_select_db(login) or die($connect_error);
+  $connect_error = 'Sorry, we\'re experiencing connection issues.';
+  mysql_connect('localhost', 'root', 'root') or die($connect_error);
+  mysql_select_db(hog) or die($connect_error);
 
   //remote
-  $connect_error = 'Sorry, we\'re experiencing connection issues.';
-  mysql_connect('db560325848.db.1and1.com', 'dbo560325848', 'kino1708') or die($connect_error);
-  mysql_select_db(db560325848) or die($connect_error);
+  // $connect_error = 'Sorry, we\'re experiencing connection issues.';
+  // mysql_connect('db560325848.db.1and1.com', 'dbo560325848', 'kino1708') or die($connect_error);
+  // mysql_select_db(db560325848) or die($connect_error);
 ?>
